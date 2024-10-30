17361b1ecbda4b1b254dbfc6d46cb96ec6bb1154
Rosela <rberberi22@epoka.edu.al>
2024-10-30 11:10:27 +0100

    fix local storage

--- OneDrive/Desktop/Exercise/3-step-form/src/hooks/useForm.js
+++ OneDrive/Desktop/Exercise/3-step-form/src/hooks/useForm.js
@@ -7,25 +7,25 @@ function useForm() {
     step3: { companyName: '', companyCode: '' },
   };
 
-  const savedStep1 = JSON.parse(localStorage.getItem('step1')) || initialData.step1;
-  const savedStep2 = JSON.parse(localStorage.getItem('step2')) || initialData.step2;
-  const savedStep3 = JSON.parse(localStorage.getItem('step3')) || initialData.step3;
-  const savedCurrentStep = parseInt(localStorage.getItem('currentStep')) || 1;
-
-  const [data, setData] = useState({
-    step1: savedStep1,
-    step2: savedStep2,
-    step3: savedStep3,
-  });
-  const [currentStep, setCurrentStep] = useState(savedCurrentStep);
+  const savedStep1 = JSON.parse(localStorage.getItem('step1'));
+  const savedStep2 = JSON.parse(localStorage.getItem('step2')) 
+  const savedStep3 = JSON.parse(localStorage.getItem('step3')) 
+  const savedCurrentStep = parseInt(localStorage.getItem('currentStep'))
+  
+  const [data, setData] = useState(initialData);
+  const [currentStep, setCurrentStep] = useState(1);
   const [submitted, setSubmitted] = useState(false);
 
- 
   useEffect(() => {
-    localStorage.setItem( `step${currentStep}`, JSON.stringify(data[`step${currentStep}`]));
-    localStorage.setItem( 'currentStep', currentStep);
-  }, [currentStep]);
-
+    if (savedStep1 || savedStep2 || savedStep3 || savedCurrentStep) {
+      setData({
+        step1: savedStep1 || initialData.step1,
+        step2: savedStep2 || initialData.step2,
+        step3: savedStep3 || initialData.step3,
+      });
+      setCurrentStep(savedCurrentStep || 1);
+    }
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -37,11 +37,19 @@ function useForm() {
       },
     }));
   };
+  
 
-  const nextStep = () => setCurrentStep((prev) => Math.min(prev + 1, 3));
-  const prevStep = () => setCurrentStep((prev) => Math.max(prev - 1, 1));
+  const nextStep = () => {
+    localStorage.setItem(`step${currentStep}`, JSON.stringify(data[`step${currentStep}`]));
+    localStorage.setItem('currentStep', currentStep + 1);
+    setCurrentStep((prev) => prev<3 ? prev+1: 3)
+  }
+
+  const prevStep = () => setCurrentStep((prev) => prev >1 ? prev-1 : 1);
 
   const submitForm = () => {
+    localStorage.setItem(`step${currentStep}`, JSON.stringify(data[`step${currentStep}`]));
+    localStorage.setItem('currentStep', currentStep + 1);
     setSubmitted(true);
   }
 
